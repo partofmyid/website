d198917cdafec2aa63416b949241e49a72f67b6d
Satria <90962949+SX-9@users.noreply.github.com>
2024-11-06 10:45:45 +0700

    form setup

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -8,6 +8,28 @@
     let available = false;
     let editable = false;
     let file = "{}";
+    let user = '';
+    let records: {
+        "A"?: string[],
+        "AAAA"?: string[],
+        "CNAME"?: string,
+        "MX"?: string[],
+        // "NS"?: string[],
+        "TXT"?: string[],
+        // "CAA"?: {
+        //     "flags": number,
+        //     "tag": string,
+        //     "value": string,
+        // }[],
+        "SRV"?: {
+            "priority": number,
+            "weight": number,
+            "port": number,
+            "target": string,
+        }[],
+        // "PTR"?: string[],
+        "ALIAS"?: string,
+    } = {};
     const search = new URLSearchParams($page.url.search);
     const q = search.get("q");
     const octokit = new Octokit({
@@ -15,15 +37,24 @@
         auth: $page.data.session?.access_token || '',
     });
 
+    function register(record: Record<string, any>) {
+        window.open('https://github.com/partofmyid/register/new/main/domains?filename=' + q + '.json'
+            + '&value=' + encodeURIComponent(JSON.stringify({
+                owner: {
+                    username: user,
+                }, record,
+            }, null, 4)));
+    }
+
     onMount(async () => {
         if (!q) return goto("/");
         const res = await fetch(`${apiBaseURL}/query/check/${q}`).then((res) => res.json());
         available = res.available;
 
-        if (!available) {
-            file = await fetch(`https://raw.githubusercontent.com/partofmyid/register/main/domains/${q}.json`).then(res => res.text());
-            const user = await octokit.rest.users.getAuthenticated()
-            if (JSON.parse(file).owner.username === user.data.login) editable = true;
+        if (!available) file = await fetch(`https://raw.githubusercontent.com/partofmyid/register/main/domains/${q}.json`).then(res => res.text());
+        if ($page.data.session) {
+            user = (await octokit.rest.users.getAuthenticated()).data.login;
+            if (JSON.parse(file).owner.username === user) editable = true;
         }
     });
 </script>
@@ -43,23 +74,22 @@
                     <input type="text" placeholder="Commit message..." class=" p-2 w-full my-2" disabled>
                 {/if}
             </div>
-            <div class="my-2">
-                {#if !available}
+            {#if !available}
+                <div class="my-2">
                     <button class="rounded-l-full bg-ctp-sapphire"><a class="no-underline text-ctp-crust" href={
+                        'https://' + q + '.part-of.my.id'
+                    }>Visit</a></button>
+                    <button class=""><a class="no-underline text-ctp-text" href={
                         'https://github.com/partofmyid/register/blob/main/domains/' + q + '.json'
                     }>File</a></button>
                     <button class="rounded-r-full"><a  class="text-ctp-text no-underline" href={
                         'https://github.com/' + JSON.parse(file)?.owner?.username
                     }>Owner</a></button>
-                {:else}
-                    <button class="rounded-full bg-ctp-sapphire"><a class="no-underline text-ctp-crust" href={
-                        'https://github.com/partofmyid/register/new/main/domains?filename=' + q + '.json'
-                    }>Register</a></button>
-                {/if}
-            </div>
+                </div>
+            {/if}
         </div>
-        {#if !available}
-            <div class="bg-ctp-base p-4">
+        <div class="bg-ctp-base p-4 h-min">
+            {#if !available}
                 <div class="flex justify-between">
                     <code>domains/{q}.json</code>
                     {#if editable}
@@ -69,7 +99,14 @@
                     {/if}
                 </div>
                 <textarea class="block" cols="30" rows={file.split('\n').length + 1} readonly={!editable} value={file} spellcheck={false}></textarea>
-            </div>
-        {/if}
+            {:else}
+                <form on:submit={() => register(records)}>
+                    <div>
+                        <!-- to be continued -->
+                    </div>
+                    <button class="rounded-full bg-ctp-sapphire text-ctp-crust" type="submit">Register</button>
+                </form>
+            {/if}
+        </div>
     </div>
 </main>
