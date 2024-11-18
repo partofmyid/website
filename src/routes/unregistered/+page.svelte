f4ebfe5d63e9b5d9988b55fd7ca282ef9382e098
SX-9 <90962949+SX-9@users.noreply.github.com>
2024-11-18 13:02:05 +0700

    subdomain display

--- src/routes/unregistered/+page.svelte
+++ src/routes/unregistered/+page.svelte
@@ -1,5 +1,15 @@
+<script>
+    import { page } from '$app/stores';
+
+    let params = new URLSearchParams($page.url.search);
+    let subdomain = params.get('d');
+</script>
+
 <main class="flex gap-4 justify-center items-center max-w-3xl mx-auto">
     <div class="flex flex-col gap-2 justify-center items-center mx-4">
+        {#if subdomain}
+            <h2>{subdomain}.part-of.my.id is unavailable</h2>
+        {/if}
         <p>If you're seeing this page, then theres a couple of possibilities:</p>
         <div class="flex flex-col gap-2 items-stretch max-w-3xl">
             <div class="bg-ctp-mantle p-4">
